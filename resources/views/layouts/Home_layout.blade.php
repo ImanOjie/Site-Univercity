aa87d9ef0b967c25a23bd2f97828799765f219a9
iman <dr.iodeveloper@gmail.com>
2023-10-05 11:50:41 +0330

    update navbar

--- resources/views/layouts/Home_layout.blade.php
+++ resources/views/layouts/Home_layout.blade.php
@@ -12,22 +12,28 @@
 </head>
 <body>
 
-<div>
-    <ul>
-        @auth
-            <form action="{{route('logout')}}" method="post">
-                @csrf
-                <li><button class="logout_btn">خروج از حساب کاربری</button></li>
-            </form>
-        @endauth
-        @guest
-            <li><a class="navbar_link" href="{{route('New_Login')}}">ورود یا ثبت نام</a></li>
-        @endguest
-        <li><a class="navbar_link" href="{{route('Manage')}}">پنل مدیریت</a></li>
-        <li><a class="navbar_link" href="{{route('List_course')}}">انتخاب واحد</a></li>
-        <li><a class="navbar_link" href="{{route('Home')}}">صفحه اصلی</a></li>
-    </ul>
+<div class="row navbar">
+    <div>
+        <ul>
+            @auth
+                <form action="{{route('logout')}}" method="post">
+                    @csrf
+                    <li><button class="logout_btn">خروج از حساب کاربری</button></li>
+                </form>
+            @endauth
+            @guest
+                <li><a class="navbar_link" href="{{route('New_Login')}}">ورود یا ثبت نام</a></li>
+            @endguest
+            <li><a class="navbar_link" href="{{route('Manage')}}">پنل مدیریت</a></li>
+            <li><a class="navbar_link" href="{{route('List_course')}}">انتخاب واحد</a></li>
+            <li><a class="navbar_link" href="{{route('Home')}}">صفحه اصلی</a></li>
+        </ul>
+    </div>
 </div>
+
+
+
+
 
 
 <div>
