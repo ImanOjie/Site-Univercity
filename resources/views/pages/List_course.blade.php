53cd5da4455be515bee03d0350aa02967ef187b1
iman <dr.iodeveloper@gmail.com>
2023-10-17 16:39:07 +0330

    update add course button css

--- resources/views/pages/List_course.blade.php
+++ resources/views/pages/List_course.blade.php
@@ -18,7 +18,7 @@
                 @endif
                 @foreach($lists as $item)
                     <tr>
-                        <td><a href="{{route('get_course',['id' => $item->id])}}"><button class="get_unit">اخذ درس</button></a></td>
+                        <td><a href="{{route('get_course',['id' => $item->id])}}"><button class="get_unit1">اخذ درس</button></a></td>
                         <td>{{$item->unit}}</td>
                         <td>{{$item->name}}</td>
                         <td>{{$loop->index+1}}</td>
@@ -44,12 +44,12 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($all_selected as $tt )
+                @foreach($all_selected as $x )
                     <tr>
-                        <td><a href="{{route('delete_getting_course',['id' => $tt->id])}}"><button class="get_unit">حذف درس</button></a></td>
-                        <td>{{$tt->Course->unit}}</td>
-                        <td>{{$tt->Course->name}}</td>
-                        <td>{{$tt->index+1}}</td>
+                        <td><a href="{{route('delete_getting_course',['id' => $x->id])}}"><button class="get_unit2">حذف درس</button></a></td>
+                        <td>{{$x->Course->unit}}</td>
+                        <td>{{$x->Course->name}}</td>
+                        <td>{{$x->index+1}}</td>
                     </tr>
                 @endforeach
                 </tbody>
