a2a6d5da15ba8c1de3ca73f666611390282ae7e5
iman <dr.iodeveloper@gmail.com>
2023-10-17 23:51:56 +0330

    add manage menu button

--- resources/views/pages/Manage.blade.php
+++ resources/views/pages/Manage.blade.php
@@ -1,14 +1,29 @@
 @extends('layouts.Manage_layout',['page_title'=>$page_title])
 @section('manage')
-    <body>
+
     <div id="wrap">
         <h1>به پنل مدیریت خوش آمدید</h1>
-
         <div id="dropdown" class="ddmenu">
-            User Settings
+            تنظیمات کاربران
             <ul>
-                <li><a href="#">My Profile</a></li>
+                <li><a href="#">لیست کاربران</a></li>
+                <li><a href="#">افزودن کاربر</a></li>
+                <li><a href="#">حذف کاربر</a></li>
+                <li><a href="#">تغییر اطلاعات کاربر</a></li>
+                <li><a href="#">تغییر درجه کاربر</a></li>
+                <li><a href="#">تغییر دسترسی کاربر</a></li>
             </ul>
         </div>
     </div>
-    </body>
+
+    <div id="wrap">
+        <div id="dropdown" class="ddmenu">
+            تنظیمات دروس
+            <ul>
+                <li><a href="#">لیست دروس</a></li>
+                <li><a href="#">افزودن درس</a></li>
+                <li><a href="#">حذف درس</a></li>
+                <li><a href="#">تغییر اطلاعات درس</a></li>
+            </ul>
+        </div>
+    </div>
