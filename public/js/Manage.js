76bbd4b22dfc99aff122fa524ad32b35a06a1905
iman <dr.iodeveloper@gmail.com>
2023-10-17 23:57:48 +0330

    update manage menu btn

--- public/js/Manage.js
+++ public/js/Manage.js
@@ -14,5 +14,22 @@ $(document).ready(function(){
     });
 
 
+    $(".drop2").on("click", function (e) {
+        e.preventDefault();
+
+        if ($(this).hasClass("open")) {
+            $(this).removeClass("open");
+            $(this).children("ul").slideUp("fast");
+        } else {
+            $(this).addClass("open");
+            $(this).children("ul").slideDown("fast");
+        }
+    });
+
+
+
+
+
+
 
 });
--- resources/views/pages/Manage.blade.php
+++ resources/views/pages/Manage.blade.php
@@ -1,29 +1,11 @@
 @extends('layouts.Manage_layout',['page_title'=>$page_title])
 @section('manage')
 
-    <div id="wrap">
-        <h1>به پنل مدیریت خوش آمدید</h1>
-        <div id="dropdown" class="ddmenu">
-            تنظیمات کاربران
-            <ul>
-                <li><a href="#">لیست کاربران</a></li>
-                <li><a href="#">افزودن کاربر</a></li>
-                <li><a href="#">حذف کاربر</a></li>
-                <li><a href="#">تغییر اطلاعات کاربر</a></li>
-                <li><a href="#">تغییر درجه کاربر</a></li>
-                <li><a href="#">تغییر دسترسی کاربر</a></li>
-            </ul>
-        </div>
-    </div>
 
-    <div id="wrap">
-        <div id="dropdown" class="ddmenu">
-            تنظیمات دروس
-            <ul>
-                <li><a href="#">لیست دروس</a></li>
-                <li><a href="#">افزودن درس</a></li>
-                <li><a href="#">حذف درس</a></li>
-                <li><a href="#">تغییر اطلاعات درس</a></li>
-            </ul>
-        </div>
-    </div>
+
+
+
+
+
+
+@endsection
