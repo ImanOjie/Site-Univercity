9ebbdff66d5154e031eb38c4f634cc73e6ff7b60
iman <dr.iodeveloper@gmail.com>
2023-10-17 21:41:17 +0330

    add manage button js

---
+++ public/js/Manage.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+    $("#dropdown").on("click", function (e) {
+        e.preventDefault();
+
+        if ($(this).hasClass("open")) {
+            $(this).removeClass("open");
+            $(this).children("ul").slideUp("fast");
+        } else {
+            $(this).addClass("open");
+            $(this).children("ul").slideDown("fast");
+        }
+    });
+
+
+
+});
