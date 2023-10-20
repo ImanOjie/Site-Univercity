9f9197f894f7330d17f7446f50852442dc7d39cc
iman <dr.iodeveloper@gmail.com>
2023-10-20 14:02:58 +0330

    update routes

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,24 @@ use Illuminate\Support\Facades\Route;
 */
 //home
 Route::get('/',[App\Http\Controllers\HomeController::class,'home'])->name('home');
-Route::middleware('auth')->get('/List_course',[App\Http\Controllers\CourseController::class,'List_course'])->name('List_course');
-Route::get('/Manage',[App\Http\Controllers\UserController::class,'Manage'])->name('Manage');
 
-//register
+//register user
 Route::get('/registering',[App\Http\Controllers\UserController::class,'registering'])->name('registering');
 Route::post('/finish_registering',[App\Http\Controllers\UserController::class,'finish_registering'])->name('finish_registering');
-
 //login user
 Route::get('/login_user',[App\Http\Controllers\UserController::class,'login_user'])->name('login_user');
 Route::post('/finishlogin_user',[App\Http\Controllers\UserController::class,'finishlogin_user'])->name('finishlogin_user');
 
-//login Manage
-Route::get('/login_manager',[App\Http\Controllers\ManageController::class,'login_manager'])->name('login_manager');
-Route::post('/finishlogin_manager',[App\Http\Controllers\ManageController::class,'finishlogin_manager'])->name('finishlogin_manager');
-
-
+//user courses
+Route::middleware('auth')->get('/List_course',[App\Http\Controllers\CourseController::class,'List_course'])->name('List_course');
 Route::get('/get_course/{id}',[App\Http\Controllers\CourseController::class,'get_course'])->name('get_course');
 Route::get('/delete_getting_course/{id}',[App\Http\Controllers\CourseController::class,'delete_getting_course'])->name('delete_getting_course');
 
+//login Manage
+Route::get('/login_manager',[App\Http\Controllers\ManageController::class,'login_manager'])->name('login_manager');
+Route::post('/finishlogin_manager',[App\Http\Controllers\ManageController::class,'finishlogin_manager'])->name('finishlogin_manager');
 //manage pages
+Route::get('/Manage',[App\Http\Controllers\ManageController::class,'Manage'])->name('Manage');
 Route::get('/Manage_list_users',[App\Http\Controllers\ManageController::class,'Manage_list_users'])->name('Manage_list_users');
 
 
