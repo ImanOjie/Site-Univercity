5d1dc457b94ac06f00a0189b4da0c0e2d4ea9629
iman <dr.iodeveloper@gmail.com>
2023-10-22 11:14:48 +0330

    add manage table list users

--- resources/views/pages/Manage_list_users.blade.php
+++ resources/views/pages/Manage_list_users.blade.php
@@ -2,7 +2,46 @@
 @section('manage')
 
     <div>
-        <h1>manage list users</h1>
+        <section class="ftco-section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6 text-center mb-5">
+                        <h2 class="heading-section">Table #09</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="table-wrap">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>زمان ثبت نام</th>
+                                    <th>رمز عبور</th>
+                                    <th>نام خانوادگی</th>
+                                    <th>نام</th>
+                                    <th>id</th>
+                                    <th>ردیف</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row"></th>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><a href="#" class="btn btn-success">Progress</a></td>
+                                </tr>
+
+
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
     </div>
 
