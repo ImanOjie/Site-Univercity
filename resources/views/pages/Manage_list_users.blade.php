7134a4e1e06067314ee4a5244955f9c0e125fcd8
iman <dr.iodeveloper@gmail.com>
2023-10-22 11:41:24 +0330

    update manage list users

--- resources/views/pages/Manage_list_users.blade.php
+++ resources/views/pages/Manage_list_users.blade.php
@@ -1,51 +1,43 @@
 @extends('layouts.Manage_layout',['page_title'=>$page_title])
 @section('manage')
-
     <div>
-        <section class="ftco-section">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-6 text-center mb-5">
-                        <h2 class="heading-section">Table #09</h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="table-wrap">
-                            <table class="table table-striped">
-                                <thead>
-                                <tr>
-                                    <th>زمان ثبت نام</th>
-                                    <th>رمز عبور</th>
-                                    <th>نام خانوادگی</th>
-                                    <th>نام</th>
-                                    <th>id</th>
-                                    <th>ردیف</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <th scope="row"></th>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                    <td><a href="#" class="btn btn-success">Progress</a></td>
-                                </tr>
-
-
-
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>زمان ثبت نام</th>
+                <th>رمز عبور</th>
+                <th>نام خانوادگی</th>
+                <th>نام</th>
+                <th>id</th>
+                <th>ردیف</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <th scope="row"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>3</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
-
-
-
-
 @endsection
