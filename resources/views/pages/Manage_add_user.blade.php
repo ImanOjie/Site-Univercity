3b1bf9c6f331d9815677e1fdcfd846727e9be7c0
iman <dr.iodeveloper@gmail.com>
2023-10-23 13:22:53 +0330

    update manage add user css

--- resources/views/pages/Manage_add_user.blade.php
+++ resources/views/pages/Manage_add_user.blade.php
@@ -1,26 +1,35 @@
 @extends('layouts.Manage_layout',['page_title'=>$page_title])
 @section('manage')
+<div class="row form">
+    <div class="col-md-7"></div>
+    <div class="col-md-4">
 
-    @if(session()->has('save_ok_shod'))
-    {{session()->get('save_ok_shod')}}
-    @endif
-    <form action="{{route('Manage_save_add_user')}}" method="post">
-        @csrf
-        @if($errors->has('name'))
-            {!! $errors->first('name') !!}
-        @endif
-        <input class="text" type="text" name="name" placeholder="نام" required="">
-        @if($errors->has('family'))
-            {!! $errors->first('family') !!}
-        @endif
-        <input class="text" type="text" name="family" placeholder="نام خانوادگی" required="">
-        @if($errors->has('password'))
-            {!! $errors->first('password') !!}
-        @endif
-        <input class="text w3lpass" type="password" name="password" placeholder="رمز عبور" required="">
+            @if(session()->has('save_ok_shod'))
+                {{session()->get('save_ok_shod')}}
+            @endif
+            <form action="{{route('Manage_save_add_user')}}" method="post">
+                @csrf
+                @if($errors->has('name'))
+                    {!! $errors->first('name') !!}
+                @endif
+                <input class="text" type="text" name="name" placeholder="نام" required="">
+                @if($errors->has('family'))
+                    {!! $errors->first('family') !!}
+                @endif
+                <input class="text" type="text" name="family" placeholder="نام خانوادگی" required="">
+                @if($errors->has('password'))
+                    {!! $errors->first('password') !!}
+                @endif
+                <input class="text" type="password" name="password" placeholder="رمز عبور" required="">
 
-        <input type="submit" value="ایجاد حساب">
-    </form>
+                <input class="submit-btn" type="submit" value="ایجاد کاربر">
+            </form>
+    </div>
+    <div class="col-md-1"></div>
+</div>
+
+
+
 
 
 @endsection
