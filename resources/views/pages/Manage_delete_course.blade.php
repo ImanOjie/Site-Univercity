b92042289470f96d7233e2e2a8065ba3f2680b85
iman <dr.iodeveloper@gmail.com>
2023-10-26 14:56:59 +0330

    manage delete course html added

--- resources/views/pages/Manage_delete_course.blade.php
+++ resources/views/pages/Manage_delete_course.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.Manage_layout',['page_title'=>$page_title])
 @section('manage')
 
+    <div class="row form">
+        <form action="{{route('Manage_save_delete_course')}}" method="post">
+            @csrf
+            @if($errors->has('name'))
+                {!! $errors->first('name') !!}
+            @endif
+            <input class="text" type="text" name="name" placeholder=" نام درس" required="">
+            <br>
+            <input class="submit-btn" type="submit" value="حذف درس">
+        </form>
+        <div class="success">
+            @if(session()->has('save_ok_shod'))
+                {{session()->get('save_ok_shod')}}
+            @endif
+        </div>
+    </div>
 
 
 
